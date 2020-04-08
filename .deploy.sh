f2986cdcd83340a55b5a709c7db2936b6ca14475
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2020-04-08 18:55:57 +0100

    Fix deletion

--- .deploy.sh
+++ .deploy.sh
@@ -28,8 +28,8 @@ function deployCopy {
   git config user.name "Travis CI"
   git config user.email "travis@openactive.org"
 
-  rm -rf
-  rm -rf .gitbook/assets
+  rm -rf activity-providers-livestream-guide-$1/*
+  rm -rf activity-providers-livestream-guide-$1/.gitbook/assets/*
 
   cd ..
 
